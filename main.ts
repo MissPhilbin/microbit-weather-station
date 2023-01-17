d4ef0c1c149779756c03bd776fdb90a29ad39612
Carrie Anne Philbin <MissPhilbin@users.noreply.github.com>
2023-01-17 13:00:05 +0000

    reduced file size

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
     basic.showString("temp")
     basic.showNumber(envirobit.getTemperature())
     if (envirobit.getTemperature() < 18) {
-        music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
         basic.showIcon(IconNames.Sad)
         basic.pause(1000)
         basic.clearScreen()
     } else if (envirobit.getTemperature() > 24) {
-        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
         basic.showIcon(IconNames.Angry)
         basic.pause(1000)
         basic.clearScreen()
     } else {
-        music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.UntilDone)
         for (let index = 0; index < 5; index++) {
             basic.showIcon(IconNames.Heart)
             basic.pause(500)
